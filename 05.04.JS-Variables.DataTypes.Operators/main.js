42841a5468f4d0ffdb87e0faffcb434b0bb87d72
KananAmir <amirovknn@gmail.com>
2023-04-05 16:22:57 +0400

    js intro

---
+++ 05.04.JS-Variables.DataTypes.Operators/main.js
@@ -0,0 +1,84 @@
+console.log("Hello World");
+console.log(7 + 5);
+
+// window.alert("test");
+
+console.log("EXRTERNAL");
+
+// let userObj = {
+//   userName: "Nabir",
+//   university: "Gdu",
+//   city: "London",
+// };
+
+// var, let, const
+
+// DATA TYPES
+
+var score;
+var cityName = "Ganja";
+
+let firstName = "Kanan";
+let lastName = "Amirov";
+
+let fullName = firstName + " " + lastName;
+
+console.log("Score: ", score);
+console.log("City: ", cityName);
+
+console.log(fullName);
+
+// let isOnline = false;
+let isOnline = true;
+
+let number1, number2;
+
+number2 = null;
+
+console.log("Number 1", number1);
+
+number1 = 90;
+console.log("Number 1", number1);
+console.log("Number 2", number2);
+cityName = "Ganja";
+
+number2 = "65";
+
+console.log("Number 2", number2);
+
+//Variables
+
+// var, let, const
+
+{
+  let uni = "GSU";
+  console.log(uni);
+}
+
+// let url = "www.code.edu.az";
+//  url = "asdasd"
+
+//  console.log(url);
+
+// var url = "www.code.edu.az";
+// var url = "asdasd"
+
+//  console.log(url);
+
+console.log(test);
+
+var test = "TEST";
+
+console.log(test);
+
+let test1 = "test";
+console.log(test1);
+
+let userName;
+
+userName = "Zarifa";
+console.log("UserName: ", userName);
+
+const BASE_URL = "https://northwind.vercel.app/api/";
+
+// BASE_URL = "www.test.com";
---
+++ 05.04.JS-Variables.DataTypes.Operators/operators.js
@@ -0,0 +1,50 @@
+// ARICHMETIC OPERATORS
+
+let firstNum = 7;
+let secondNum = 3;
+
+let sum, multiply, subtruct, division, reminder;
+
+sum = firstNum + secondNum;
+multiply = firstNum * secondNum;
+subtruct = firstNum - secondNum;
+division = firstNum / secondNum;
+reminder = firstNum % secondNum;
+power = firstNum ** secondNum;
+
+// console.log(sum, multiply);
+console.log("SUM: ", sum);
+console.log("DIVISION: ", division);
+console.log("MULTIPLY: ", multiply);
+console.log("SUBTRUCT: ", subtruct);
+console.log("REMINDER: ", reminder);
+console.log("POWER: ", power);
+
+let counter = 0;
+
+// counter++;
+// counter = counter + 1;
+
+counter++;
+// counter = counter - 1;
+console.log(++counter);
+console.log(counter);
+
+// console.log(counter);
+
+let num = 5;
+
+num += 3;
+// num = num + 3;
+
+console.log(num);
+
+//Bitwise Assignment Operators
+
+console.log("5 & 7 : ", 5 & 7);
+console.log("5 | 7 : ", 5 | 7);
+
+// Shift Assignment Operators
+
+console.log("5 >> 1 : ", 5 >> 1);
+console.log("5 << 1 : ", 5 << 1);
